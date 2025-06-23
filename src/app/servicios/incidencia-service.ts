da0c88cce94f64b178a9fcb5cfd2de67c52fd5d5
Hackerman69 <raulhenares5@gmail.com>
2025-06-23 17:12:08 +0200

    final dia 6

--- src/app/servicios/incidencia-service.ts
+++ src/app/servicios/incidencia-service.ts
@@ -1,7 +1,6 @@
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
-import { Incidencia } from '../models/incidencia';
 
 @Injectable({
   providedIn: 'root'
